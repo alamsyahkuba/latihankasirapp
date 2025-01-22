ac4db45747cca2f85acbe86855c0e895c1931d98
alamsyahkuba <alam.xpplg2@gmail.com>
2025-01-22 07:48:41 +0700

    menambahkan fitur read dan search product

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -4,103 +4,109 @@ import 'theme.dart';
 import 'package:latihankasirapp/pages/welcomepages.dart';
 import 'package:latihankasirapp/pages/homeappbar.dart';
 import 'package:latihankasirapp/pages/itemwidget.dart';
+
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
   _HomePageState createState() => _HomePageState();
-  }
+}
 
 class _HomePageState extends State<HomePage> {
+  String searchQuery = ''; // Menyimpan input search dari pengguna
+
   @override
-  Widget build (BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView(children: [
-        HomeAppBar(),
-        Container(
-          //ukuran tinggi sementara
-          padding: EdgeInsets.only(top: 15),
-          decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(35),
-              topRight: Radius.circular(35),
+      body: ListView(
+        children: [
+          HomeAppBar(),
+          Container(
+            //ukuran tinggi sementara
+            padding: EdgeInsets.only(top: 15),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(35),
+                topRight: Radius.circular(35),
+              ),
             ),
-          ),
-          child: Column(
-            children: [
-              Container(
-                margin: EdgeInsets.symmetric(horizontal: 15),
-                padding: EdgeInsets.symmetric(horizontal: 15),
-                height: 50,
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(30),
-                ),
-                child: Row(
-                  children: [
-                    Container(
-                      margin: EdgeInsets.only(left: 5),
-                      height: 50,
-                      width: 300,
-                      child: TextFormField(
-                        decoration: InputDecoration(
-                          border: InputBorder.none,
-                          hintText: "Ketik untuk cari..."
+            child: Column(
+              children: [
+                Container(
+                  margin: EdgeInsets.symmetric(horizontal: 15),
+                  padding: EdgeInsets.symmetric(horizontal: 15),
+                  height: 50,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                  child: Row(
+                    children: [
+                      Container(
+                        margin: EdgeInsets.only(left: 5),
+                        height: 50,
+                        width: 300,
+                        child: TextFormField(
+                          decoration: InputDecoration(
+                              border: InputBorder.none,
+                              hintText: "Ketik untuk cari..."),
+                          onChanged: (value) {
+                            setState(() {
+                              // Mengubah value dari pengguna menjadi lowercase
+                              searchQuery = value.toLowerCase();
+                            });
+                          },
                         ),
                       ),
+                      Spacer(),
+                      Icon(
+                        Icons.search,
+                        size: 27,
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+          //item
+          Container(
+            alignment: Alignment.centerLeft,
+            margin: EdgeInsets.symmetric(vertical: 20, horizontal: 10),
+            child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text("Daftar Produk",
+                      style: sixTextStyle.copyWith(
+                        fontSize: 18,
+                      )),
+                  ElevatedButton(
+                    onPressed: () {
+                      print("Tambah Produk");
+                    },
+                    style: ElevatedButton.styleFrom(
+                      shape: CircleBorder(),
+                      padding: EdgeInsets.all(10),
+                      backgroundColor: fourthColor,
                     ),
-                    Spacer(),
-                    Icon(
-                      Icons.search,
-                      size: 27,
-                      
-                    )
-                  ],
-                ),
-              )
-            ],
+                    child: Icon(
+                      Icons.add,
+                      color: Colors.white,
+                    ),
+                  )
+                ]),
           ),
-        ),
-        //item
-        Container(
-          alignment: Alignment.centerLeft,
-          margin: EdgeInsets.symmetric(vertical: 20, horizontal: 10),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text(
-            "Daftar Produk",
-            style: sixTextStyle.copyWith(
-              fontSize: 18,
-            )
-          ),
-          ElevatedButton(
-            onPressed: () {
-              print("Tambah Produk");
-            },
-            style: ElevatedButton.styleFrom(
-              shape: CircleBorder(),
-              padding: EdgeInsets.all(10),
-              backgroundColor: fourthColor,
-            ),
-            child: Icon(
-              Icons.add,
-              color: Colors.white,
-            ),
-          )
-          ]
-          ),
-        ),
-        //Item Widget
-        ItemWidget(),
-      ],
+          //Item Widget
+          ItemWidget(searchQuery: searchQuery),
+          // Memanggil ItemWidget dengan parameter searchQuery (yang bersifat required) dengan nilai searchQuery yang berisi inputan search dari pengguna
+        ],
       ),
       bottomNavigationBar: CurvedNavigationBar(
         backgroundColor: Colors.transparent,
-        buttonBackgroundColor: fourthColor,  
+        buttonBackgroundColor: fourthColor,
         onTap: (index) {},
-        height: 70,    
+        height: 70,
         color: secondaryColor,
         items: [
           Icon(
@@ -132,6 +138,4 @@ class _HomePageState extends State<HomePage> {
       ),
     );
   }
-  }
-  
-  
+}
